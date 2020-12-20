bb13fab842ded38c97ea9d0a6a50216627c1c116
Matt Patrick <hazmat345@gmail.com>
2020-12-19 23:38:52 -0500

    Adding jinja example

---
+++ jinja/jinja/__main__.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+from brewtils import command, system, Plugin, parameter
+from brewtils.models import Parameter
+from .__version__ import __version__
+
+
+class Nav:
+    href = Parameter(key="href", type="String", optional=False)
+    caption = Parameter(key="caption", type="String", optional=False)
+
+    parameters = [href, caption]
+
+
+@system
+class Client:
+
+    def __init__(self):
+        self._env = Environment(
+            loader=FileSystemLoader('jinja/resources'), autoescape=True
+        )
+
+    @parameter(key="nav", model=Nav, multi=True)
+    @parameter(key="a_variable", type="String")
+    @command(output_type="HTML")
+    def do_jinja(self, a_variable=None, nav=None):
+        template = self._env.get_template('test_template.j2')
+
+        return template.render(navigation=nav, a_variable=a_variable)
+
+
+def main():
+    plugin = Plugin(
+        name="jinja",
+        version=__version__,
+        description="",
+    )
+    plugin.client = Client()
+    plugin.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ jinja/setup.py
@@ -0,0 +1,37 @@
+import re
+
+from setuptools import setup
+
+
+def find_version(version_file):
+    version_line = open(version_file, "rt").read()
+    match_object = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_line, re.M)
+
+    if not match_object:
+        raise RuntimeError("Unable to find version string in %s" % version_file)
+
+    return match_object.group(1)
+
+
+setup(
+    name="jinja",
+    version=find_version("jinja/__version__.py"),
+    description="Plugin that demonstrates Jinja2 templates",
+    url="https://beer-garden.io",
+    author="The Beergarden Team",
+    author_email=" ",
+    license="MIT",
+    packages=["jinja"],
+    include_package_data=True,
+    install_requires=["brewtils"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
